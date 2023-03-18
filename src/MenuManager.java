2f5b0691154f1e52ae1c22370d11a6d050345e6e
jeonghuido <galaxy9103@naver.com>
2023-03-18 15:59:09 +0900

    removed hi.java and added menumanage.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add schedule");
+		System.out.println("2. Delet schedule");
+		System.out.println("3. Edit schedule");
+		System.out.println("4. View schedule");
+		System.out.println("5. Exit");
+	}
+
+}
