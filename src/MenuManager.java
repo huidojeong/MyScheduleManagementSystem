1088c6c7b755359aa36bfe1113e36de045c8aca3
jeonghuido <galaxy9103@naver.com>
2023-03-18 16:23:09 +0900

    added title

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,6 +2,7 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
+		System.out.println("**** Schedule Management System Menu ****");
 		System.out.println("1. Add schedule");
 		System.out.println("2. Delet schedule");
 		System.out.println("3. Edit schedule");
