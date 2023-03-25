871d1805d5550c588cd49c65a130dcd1d2494418
jeonghuido <galaxy9103@naver.com>
2023-03-26 00:08:09 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,87 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("**** Schedule Management System Menu ****");
-		System.out.println("1. Add schedule");
-		System.out.println("2. Delet schedule");
-		System.out.println("3. Edit schedule");
-		System.out.println("4. View schedule");
-		System.out.println("5. Exit");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num != 6) {
+			System.out.println("**** Drama Schedule Management System Menu ****");
+			System.out.println("1. Add Drama schedule");
+			System.out.println("2. Delet Drama schedule");
+			System.out.println("3. Edit Drama schedule");
+			System.out.println("4. View Drama schedule");
+			System.out.println("5. show a menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number betwen 1 - 6:");
+			num = input.nextInt();
+			if (num == 1) {
+				addDramaSchedule();
+			}
+			else if (num == 2) {
+				deletDramaSchedule();
+			}
+			else if (num == 3) {
+				editDramaSchedule();
+			}
+			else if (num == 4) {
+				viewDramaSchedule();
+			}	
+			else {
+				continue;
+			}
+		}
+	}	
+	
+	
+	public static void addDramaSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("drama name:");
+		String dramaname = input.next();
+		System.out.print("drama channel name");
+		String dramachannel = input.next();
+		System.out.print("drama running time(Minutes)");
+		int runningtime = input.nextInt();
+		System.out.print("The number of episodes in a drama");
+		int episodes = input.nextInt();
+		
 	}
-
+	
+	public static void deletDramaSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("drama name:");
+		String dramaname = input.next();
+		System.out.print("drama channel name");
+		String dramachannel = input.next();
+		System.out.print("drama running time(Minutes)");
+		int runningtime = input.nextInt();
+		System.out.print("The number of episodes in a drama");
+		int episodes = input.nextInt();
+	}
+	
+	
+	public static void editDramaSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("drama name:");
+		String dramaname = input.next();
+		System.out.print("drama channel name");
+		String dramachannel = input.next();
+		System.out.print("drama running time(Minutes)");
+		int runningtime = input.nextInt();
+		System.out.print("The number of episodes in a drama");
+		int episodes = input.nextInt();
+	}
+	
+	public static void viewDramaSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("drama name:");
+		String dramaname = input.next();
+		System.out.print("drama channel name");
+		String dramachannel = input.next();
+		System.out.print("drama running time(Minutes)");
+		int runningtime = input.nextInt();	
+		System.out.print("The number of episodes in a drama");
+		int episodes = input.nextInt();
+	}		
 }
