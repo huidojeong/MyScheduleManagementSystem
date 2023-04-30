bf1f0477048402f96c91d91d4d01cf6882a4d03e
jeonghuido <galaxy9103@naver.com>
2023-04-30 19:41:05 +0900

    update 5week assignment

--- src/DramaManager.java
+++ src/DramaManager.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import drama.DramaSchedule;
+import drama.HistoricalDramaSchedule;
+
 public class DramaManager {
    ArrayList<DramaSchedule> dramaSchedules = new ArrayList<DramaSchedule>();
    Scanner input;
@@ -9,27 +12,37 @@ public class DramaManager {
    }
    
    public void addDramaSchedule() {
-      DramaSchedule dramaSchedule = new DramaSchedule();
-      System.out.print("drama name:");
-      dramaSchedule.dramaname = input.next();
-      
-      System.out.print("drama channel name");
-      dramaSchedule.dramachannel = input.next();
-      
-      System.out.print("drama running time(Minutes)");
-      dramaSchedule.runningtime = input.nextInt();
-      
-      System.out.print("The number of episodes in a drama");
-      dramaSchedule.episodes = input.nextInt();
-      dramaSchedules.add(dramaSchedule);
+	   int kind=0;
+	   DramaSchedule dramaSchedule;
+	   while (kind !=1&kind!=2) {
+		   System.out.print("1 for Romance Drama"); 
+		   System.out.print("2 for Historical Drama");
+		   System.out.print("Select num for drama kind between 1 and 2:"); 
+		   kind = input.nextInt();
+		   if (kind==1) {
+			   dramaSchedule = new DramaSchedule();
+			   dramaSchedule.getUserInput(input);
+			   dramaSchedules.add(dramaSchedule);
+			   break;
+		   }
+		   else if(kind ==2) {
+			   dramaSchedule = new HistoricalDramaSchedule();
+			   dramaSchedule.getUserInput(input);
+			   dramaSchedules.add(dramaSchedule);
+			   break;
+		   }
+		   else {
+			   System.out.print("Select num for drama kind:"); 
+		   }
+	   }
    }
-   
+
    public void deletDramaSchedule() {
       System.out.print("drama name:");
       String dramaname = input.next();
       int index = -1;
       for(int i=0; i<dramaSchedules.size();i++) {
-    	  if(dramaSchedules.get(i).dramaname.equals(dramaname)) {
+    	  if(dramaSchedules.get(i).getDramaname().equals(dramaname)) {
     		  index = i;
     		  break;
     	  }
@@ -52,7 +65,7 @@ public class DramaManager {
       String dramaname = input.next();
       for(int i=0; i<dramaSchedules.size();i++) {
     	  DramaSchedule dramaschedule = dramaSchedules.get(i);
-    	  if(dramaschedule.dramaname.equals(dramaname)) {
+    	  if(dramaschedule.getDramaname().equals(dramaname)) {
     		  int num = -1;
 
     		  while(num != 5) {
@@ -67,22 +80,26 @@ public class DramaManager {
 
     			  if (num == 1) {
     				  System.out.print("dramaname: ");
-    				  dramaschedule.dramaname = input.next();
+    				  String dramaname1 = input.next();
+    				  dramaschedule.setDramaname(dramaname1);
     			  }
 
     			  else if (num == 2) {
     				  System.out.print("dramachannel: ");
-    				  dramaschedule.dramachannel = input.next();
+    				  String dramachannel = input.next();
+    				  dramaschedule.setDramachannel(dramachannel);
     			  }
 
     			  else if (num == 3) {
     				  System.out.print("runningtime: ");
-    				  dramaschedule.runningtime = input.nextInt();
+    				  int runningtime = input.nextInt();
+    				  dramaschedule.setRunningtime(runningtime);
     			  }
 
     			  else if (num == 4) {
     				  System.out.print("episodes: ");
-    				  dramaschedule.episodes = input.nextInt();
+    				  int episodes = input.nextInt();
+    				  dramaschedule.setEpisodes(episodes);
     			  }
 
     			  else {
@@ -98,7 +115,8 @@ public class DramaManager {
    
    public void viewDramaSchedule() {
 //      System.out.print("drama name:");
-//	  String dramaname = input.next();
+//	  String dramaname1 = input.next();
+	  System.out.println("#of registered students: " + dramaSchedules.size());
 	  for(int i=0; i<dramaSchedules.size();i++) {
 		  dramaSchedules.get(i).printInfo();
 	  }
